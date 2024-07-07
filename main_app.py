1c2f07ce7eec7965c504d7665ea48bbe035644c3
Mr-T18 <itsuki.tkhs@gmail.com>
2024-07-07 16:10:12 +0900

    third commit

--- main_app.py
+++ main_app.py
@@ -1,8 +1,10 @@
 import streamlit as st
 from PIL import Image
+import os
 
 st.title("Tのアプリ")
 st.caption("これはTのテストアプリです")
 
-image = Image.open("./data/image1.png")
+path = os.path.dirname(__file__)
+image = Image.open(path + "/data/image1.png")
 st.image(image,width=200)
--- pages/page3.py
+++ pages/page3.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import pandas as pd
+import os
 
 #データ分析関連
-df = pd.read_csv("./data/平均気温.csv",index_col="月")
+df_path = os.path.dirname(os.path.dirname(__file__))
+df = pd.read_csv(df_path + "/data/平均気温.csv",index_col="月")
 st.dataframe(df)
 st.line_chart(df)
 st.bar_chart(df["2023年"])
